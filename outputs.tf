7c3b768e1f0cf1ec50dbb93baaf63c149d6446b3
Nagy Claudiu <claudiuvasile.nagy@eumetsat.int>
2024-07-26 11:23:39 +0000

    expose instance id

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,7 @@
 output "instance-data" {
   value = {
     "name" = openstack_compute_instance_v2.instance.name
+    "id" = openstack_compute_instance_v2.instance.id
     "ip"   = openstack_compute_instance_v2.instance.access_ip_v4
     "floatingip" = try(openstack_networking_floatingip_v2.fip[0].address, null) != null ? openstack_networking_floatingip_v2.fip[0].address : null
   }
